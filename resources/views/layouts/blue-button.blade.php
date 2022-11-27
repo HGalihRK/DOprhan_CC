2fd44d3b4fa0a8ab66526285113d2748cbfad348
galih <haning.grk@gmail.com>
2022-11-27 21:20:37 +0700

    a

---
+++ resources/views/layouts/blue-button.blade.php
@@ -0,0 +1,3 @@
+<button type="button"
+class="inline-flex justify-center items-center space-x-2 rounded font-bold focus:outline-none px-3 py-2 leading-6 bg-blue-500 text-white hover:bg-blue-600 focus:ring focus:ring-blue-500 focus:ring-opacity-50 active:bg-blue-500 active:border-blue-500">
+{{$slot}}</button>
