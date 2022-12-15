87517493578a4d227cad0f0996abcdf153d3fcd7
Nathanael Abel <yujioshinozaki@gmail.com>
2022-12-15 22:37:02 +0700

    OrphanageController updated and more orphs data

--- database/seeders/OrphanageSeeder.php
+++ database/seeders/OrphanageSeeder.php
@@ -16,16 +16,11 @@ class OrphanageSeeder extends Seeder
     public function run()
     {
         //array ini berisikan data asli panti asuhan dari nomor 1-34
-        $arrayName = ['Panti Asuhan BJ Habibie', 'Panti Asuhan Al Kahfi', 'Panti Asuhan Al Qomariyah', 'Panti Asuhan Ibnu Hajar',
-                        'Panti Asuhan Amanah Insan', 'Panti Asuhan Diponegoro', 'Panti Asuhan Tanwir', 'Panti Asuhan At Taqwa',
-                        'Panti Asuhan Sejahtera', 'Panti Asuhan Muhammadiyah Kenjeran', 'Panti Asuhan Muhammadiyah Kh. Achmad Dahlan',
-                        'Panti Asuhan Al Amal', 'Panti Asuhan Ppay Al Amal Putri', 'Panti Asuhan Lil Wathon', 'Panti Asuhan Rodhiyatul Jannah',
-                        'Panti Asuhan Jamiyah Muslimat Az Zahara', 'Panti Asuhan Al Ikhlas', 'Panti Asuhan Khadijah 2', 'Panti Asuhan Al Fatah',
-                        'Panti Asuhan Mitra Arafah', 'Panti Asuhan Mahbubiyah', 'Panti Asuhan Al Ikhlas', 'Panti Asuhan Muhammadiyah Pakis',
-                        'Panti Asuhan Muhammadiyah Putat Jaya', 'Panti Asuhan Muhammadiyah Karang Pilang', 'Panti Asuhan Putri Aisyiyah 2',
-                        'Panti Asuhan Yatim & Dhuafa Al Muttaqin', 'Panti Asuhan Darul Aitam Sidogiri', 'Panti Asuhan Don Bosco',
-                        'Panti Asuhan Bani Yaqub', 'Yayasan Panti Asuhan Sejahtera', 'Panti Asuhan Yatim Cahaya Insani',
-                        'Panti Asuhan Undaan Surabaya', 'Panti Asuhan Ada Hari Esok', ];
+        $arrayName = [
+            'Panti Asuhan BJ Habibie', 'Panti Asuhan Al Kahfi', 'Panti Asuhan Al Qomariyah', 'Panti Asuhan Ibnu Hajar', 'Panti Asuhan Amanah Insan', 'Panti Asuhan Diponegoro', 'Panti Asuhan Tanwir', 'Panti Asuhan At Taqwa', 'Panti Asuhan Sejahtera', 'Panti Asuhan Muhammadiyah Kenjeran', 'Panti Asuhan Muhammadiyah Kh. Achmad Dahlan',
+            'Panti Asuhan Al Amal', 'Panti Asuhan Ppay Al Amal Putri', 'Panti Asuhan Lil Wathon', 'Panti Asuhan Rodhiyatul Jannah', 'Panti Asuhan Jamiyah Muslimat Az Zahara', 'Panti Asuhan Al Ikhlas', 'Panti Asuhan Khadijah 2', 'Panti Asuhan Al Fatah', 'Panti Asuhan Mitra Arafah', 'Panti Asuhan Mahbubiyah', 'Panti Asuhan Al Ikhlas', 'Panti Asuhan Muhammadiyah Pakis', 'Panti Asuhan Muhammadiyah Putat Jaya', 'Panti Asuhan Muhammadiyah Karang Pilang', 'Panti Asuhan Putri Aisyiyah 2',
+            'Panti Asuhan Yatim & Dhuafa Al Muttaqin', 'Panti Asuhan Darul Aitam Sidogiri', 'Panti Asuhan Don Bosco', 'Panti Asuhan Bani Yaqub', 'Yayasan Panti Asuhan Sejahtera', 'Panti Asuhan Yatim Cahaya Insani', 'Panti Asuhan Undaan Surabaya', 'Panti Asuhan Ada Hari Esok', 'Asrama Panti Baitul Yatim', 'Panti Asuhan Yatim Piatu Al Mu Min', 'Panti Asuhan Arief Rahman Hakim', 'Rumah Anak Pondok Kasih', 'Panti Asuhan Muhammadiyah Karangpilang', 'Panti Asuhan Ashabul Kahfi', 'Yayasan Islam Nurul Hasanah', 'Yayasan Panti Asuhan Al Ikhsan', 'Yayasan Panti Asuhan Ibma Mmyam', 'Panti Asuhan Al Mizan', 'Panti Asuhan Ibnu Sina Kertajaya', 'Panti Asuhan Muhammadiyah Medokan Ayu', 'Panti Asuhan Karya Asih', 'Panti Asuhan Pelayan Kasih', 'Panti Asuhan Sumber Kasih', 'Panti Asuhan Muhammadiyah Wiyung', 'Panti Asuhan Muslim Surabaya', 'Panti Asuhan Cinta Kasih Theresa', 'Panti Asuhan Santa Yulia', 'Panti Asuhan Al - Hasyimi', 'Panti Asuhan Yatim Ashabul Kahfi', 'Panti Asuhan Hajjah Jawiyah Badrie', 'Panti Asuhan Penerus Kasih', 'Panti Asuhan Darussalam', 'Panti Asuhan Yatim Darul Hiunah', 'Panti Asuhan Ulul Albab - Tenggilis', 'Panti Asuhan Rif atus Sholihah'
+        ];
         $i = 0;
         foreach (User::all()->where('user_type', 'Pengurus Panti') as $user) {
             Orphanage::factory()->create([
