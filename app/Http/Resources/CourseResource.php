69da38a5b9cebde2fe30b9bc779ee6016f2721b5
Nur Azizah <nur.azizah.7241@gmail.com>
2022-12-15 22:43:50 +0700

    fixing api competitions

--- app/Http/Resources/CourseResource.php
+++ app/Http/Resources/CourseResource.php
@@ -9,11 +9,25 @@ class CourseResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'course' => $this->course,
+            'hour_count' => $this->hour_count,
+            'start_date' => $this->start_date,
+            'status' => $this->status,
+            'total_price' => $this->total_price,
+            'transaction' => $this->transaction,
+            'orphanage' => $this->orphanage,
+            'orphans' => Orphan::whereIn('id', $this->orphanCourseBookings->pluck('orphan_id'))->get(),
+            'schedule' => DayTimeRange::whereIn('id', $this->courseBookingDayTimeRanges->pluck('day_time_range_id'))->get(),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
     }
 }
