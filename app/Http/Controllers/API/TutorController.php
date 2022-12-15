d10acbb20114e1c97b73653fb85798a01793e34a
Nathanael Abel <yujioshinozaki@gmail.com>
2022-12-15 22:40:03 +0700

    Update TutorController.php

--- app/Http/Controllers/API/TutorController.php
+++ app/Http/Controllers/API/TutorController.php
@@ -24,6 +24,26 @@ class TutorController extends Controller
         if ($request->userId) {
             return ['result' => TutorResource::collection(Tutor::where('user_id', $request->userId)->get())];
         }
+        // return tutor by orphan id
+        if ($request->orphanId) {
+            return ['result' => TutorResource::collection(Tutor::where('orphan_id', $request->orphanId)->get())];
+        }
+        // return tutor by id and user id
+        if ($request->id && $request->userId) {
+            return ['result' => TutorResource::collection(Tutor::where('id', $request->id)->where('user_id', $request->userId)->get())];
+        }
+        // return tutor by id and orphan id
+        if ($request->id && $request->orphanId) {
+            return ['result' => TutorResource::collection(Tutor::where('id', $request->id)->where('orphan_id', $request->orphanId)->get())];
+        }
+        // return tutor by user id and orphan id
+        if ($request->userId && $request->orphanId) {
+            return ['result' => TutorResource::collection(Tutor::where('user_id', $request->userId)->where('orphan_id', $request->orphanId)->get())];
+        }
+        // return tutor by id, user id and orphan id
+        if ($request->id && $request->userId && $request->orphanId) {
+            return ['result' => TutorResource::collection(Tutor::where('id', $request->id)->where('user_id', $request->userId)->where('orphan_id', $request->orphanId)->get())];
+        }
         // return all tutor if id not exist
         return ['result' => TutorResource::collection(Tutor::all())];
     }
