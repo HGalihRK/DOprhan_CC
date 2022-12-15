ed8dba976e4105c20e7b6985007ace03889db481
Nur Azizah <nur.azizah.7241@gmail.com>
2022-12-15 23:23:15 +0700

    .

--- app/Http/Controllers/API/CompetitionController.php
+++ app/Http/Controllers/API/CompetitionController.php
@@ -2,27 +2,23 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Requests\StoreCompetitionRequest;
-use App\Http\Requests\UpdateCompetitionRequest;
 use App\Http\Resources\CompetitionResource;
 use App\Models\Competition;
 use Illuminate\Http\Request;
 
 class CompetitionController extends Controller
 {
-  /**
-   * Display a listing of the resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function index(Request $request)
-  {
-    if ($request->id) {
-      return ['result' => CompetitionResource::collection(Competition::where('id', $request->id)->get())];
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        if ($request->id) {
+            return ['result' => CompetitionResource::collection(Competition::where('id', $request->id)->get())];
+        }
+
+        return ['result' => CompetitionResource::collection(Competition::all())];
     }
-    if ($request->skill_id) {
-      return ['result' => CompetitionResource::collection(Competition::where('skill_id', $request->skill_id)->get())];
-    }
-    return ['result' => CompetitionResource::collection(Competition::all())];
-  }
 }
