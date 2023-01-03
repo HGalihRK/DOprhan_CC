7ecebd2fd5cbe91311e69944f7063ef24b1fc64c
Nur Azizah <nur.azizah.7241@gmail.com>
2023-01-04 06:47:16 +0700

    .

--- resources/views/components/api-description-item.blade.php
+++ resources/views/components/api-description-item.blade.php
@@ -1,5 +1,5 @@
 <tr class="odd:bg-white even:bg-gray-100">
-    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900">GET</td>
+    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900">GET/HEAD</td>
     <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-700">{{ $parameter }}</td>
     <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-700">{{ $wajib }}</td>
     <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-700">{{ $tipe }}</td>
